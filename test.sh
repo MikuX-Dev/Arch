40d4ba792bb910dc5a5002710a4de24c14ff4d28
MikuX-Dev <abinishanth3464@gmail.com>
2023-11-22 19:44:22 +0530

    changed "s" , and reflector from "a" to "l"

--- test.sh
+++ test.sh
@@ -20,17 +20,17 @@ echo -ne "
 │       and enjoy         │
 ╰─────────────────────────╯
 "
-echo "Arch Linux Fast Install (ArchFiery) - Version: 2023.11.06 (GPL-3.0)"
-sleep 5s
+echo "Arch Linux Fast Install (ArchFiery) - Version: 2023.11.22 (GPL-3.0)"
+sleep 6s
 printf "\n"
 echo "Installation guide starts now..."
-sleep 5s
+sleep 6s
 clear
 
 # install package
 echo "Installing needed packages..."
 pacman -Syy reflector rsync curl --noconfirm
-sleep 5s
+sleep 6s
 clear
 
 # Installing fastest mirrors
@@ -44,19 +44,19 @@ if [[ $fm =~ ^[Yy]$ ]]; then
   # Archlinux mirrorlists
   printf "\n"
   echo "Archlinux-mirrorlist setup"
-  reflector --verbose -a 22 -f 22 -p https --download-timeout 55 --sort rate --save /etc/pacman.d/mirrorlist
+  reflector --verbose -l 22 -f 22 -p https --download-timeout 55 --sort rate --save /etc/pacman.d/mirrorlist
   pacman -Syy
 else
   printf "\n"
   echo "Skipping the fastert mirrorlist"
 fi
-sleep 5s
+sleep 6s
 clear
 
 # Setting up system clock
 echo "Ensuring if the system clock is accurate."
 timedatectl set-ntp true
-sleep 5s
+sleep 6s
 clear
 
 # # Setting up drive
@@ -66,7 +66,7 @@ clear
 # printf "\n"
 # echo "Enter the drive to install arch linux on it. (/dev/...)"
 # read -p "Enter Drive (eg. /dev/sda or /dev/vda or /dev/nvme0n1 or something similar): " drive
-# sleep 5s
+# sleep 6s
 # clear
 
 # echo "Getting ready for creating partitions!"
@@ -106,13 +106,13 @@ clear
 #   ;;
 # esac
 # echo ""$partitionutility" is the selected disk utility tool for partition."
-# sleep 5s
+# sleep 6s
 # clear
 
 # echo "Getting ready for formatting partitions!"
 # printf "\n"
 # "$partitionutility" "$drive"
-# sleep 5s
+# sleep 6s
 # clear
 
 # printf "\n"
@@ -146,11 +146,11 @@ clear
 #   ;;
 # esac
 # echo ""$filesystemtype" is the selected file system type."
-# sleep 5s
+# sleep 6s
 # clear
 
 # echo "Getting ready for formatting drives."
-# sleep 5s
+# sleep 6s
 # printf "\n"
 # lsblk
 # printf "\n"
@@ -204,7 +204,7 @@ clear
 # else
 #   echo "Using the EFI partition..."
 # fi
-# sleep 5s
+# sleep 6s
 # clear
 
 # Function to prompt user for input with default value
@@ -345,13 +345,13 @@ clear
 sync
 printf "\n"
 lsblk
-sleep 5s
+sleep 6s
 clear
 
 # Installing base system...
 echo "Installing Base system..."
 printf "\n"
-sleep 5s
+sleep 6s
 
 # Determine processor type and install microcode
 determine_processor_type() {
@@ -404,7 +404,7 @@ echo "Generating fstab file"
 printf "\n"
 genfstab -U /mnt >>/mnt/etc/fstab
 cat /mnt/etc/fstab
-sleep 5s
+sleep 6s
 clear
 
 # Setup for post-installation
@@ -412,25 +412,25 @@ sed '1,/^#part2$/d' test.sh >/mnt/post_install.sh
 chmod +x /mnt/post_install.sh
 arch-chroot /mnt ./post_install.sh
 clear
-sleep 5s
+sleep 6s
 
 # Unmount drives
 echo "unmounting all the drives"
 printf "\n"
 umount -R /mnt
-sleep 5s
+sleep 6s
 clear
 
 echo -ne "
 ╭─────── ArchFiery ───────╮
 │       Installation      │
 │        completed        │
-│    rebooting in 15s     │
+│    rebooting in 19s     │
 ╰─────────────────────────╯
 "
 printf "\n"
-echo "Installation Finished. REBOOTING IN 9 SECONDS!!!"
-sleep 15s
+echo "Installation Finished. REBOOTING IN 19 SECONDS!!!"
+sleep 19s
 reboot
 
 #part2
@@ -439,10 +439,10 @@ reboot
 echo -ne "
 ╭─────── ArchFiery ───────╮
 │      post_install       │
-│     starting in 10s     │
+│     starting in 9s      │
 ╰─────────────────────────╯
 "
-sleep 10s
+sleep 9s
 clear
 
 # Check if multilib and community repositories are enabled
@@ -455,14 +455,14 @@ else
   # Repositories are not enabled, add them
   echo -e "\n[community]\nInclude = /etc/pacman.d/mirrorlist\n\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >>/etc/pacman.conf
 fi
-sleep 5s
+sleep 6s
 clear
 
 # Setting up pacman
 echo "Setting up pacman"
 sed -i 's/#Color/Color/g' /etc/pacman.conf
 sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5\nILoveCandy/' /etc/pacman.conf
-sleep 5s
+sleep 6s
 clear
 
 # Installing fastest mirrors
@@ -474,38 +474,38 @@ if [[ $fm =~ ^[Yy]$ ]]; then
   cp -r /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
   # Archlinux mirrorlists
   echo "Archlinux-mirrorlist setup"
-  reflector --verbose -a 22 -f 22 -p https --download-timeout 55 --sort rate --save /etc/pacman.d/mirrorlist
+  reflector --verbose -l 22 -f 22 -p https --download-timeout 55 --sort rate --save /etc/pacman.d/mirrorlist
   pacman -Syy
 else
   echo "Skipping the fastert mirrorlist"
 fi
-sleep 5s
+sleep 6s
 clear
 
 # Replace Asia/kolkata with your timezone
 echo "Setting timezone"
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
-sleep 5s
+sleep 6s
 clear
 
 # Gen locale
 echo "Generating locale"
 sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
 locale-gen
-sleep 5s
+sleep 6s
 clear
 
 # Setting lang
 echo "Setting LANG variable"
 echo "LANG=en_US.UTF-8" >/etc/locale.conf
-sleep 5s
+sleep 6s
 clear
 
 # Setting console keyboard
 echo "Setting console keyboard layout"
 echo 'KEYMAP=us' >/etc/vconsole.conf
-sleep 5s
+sleep 6s
 clear
 
 # Setup hostname
@@ -516,7 +516,7 @@ printf "\n"
 echo "Checking hostname (/etc/hostname)"
 printf "\n"
 cat /etc/hostname
-sleep 5s
+sleep 6s
 clear
 
 # Setting up hosts
@@ -528,14 +528,14 @@ printf "\n"
   echo "127.0.1.1       $hostname"
 } >>/etc/hosts
 cat /etc/hosts
-sleep 5s
+sleep 6s
 clear
 
 # Install pkgs and tools by PACMAN..
 echo "Installing pkgs and tools by PACMAN.."
-sleep 5s
+sleep 6s
 pacman -S --noconfirm --needed ack acpi adobe-source-sans-pro-fonts alacritty alsa-utils amd-ucode android-file-transfer android-tools apache arch-install-scripts arch-wiki-docs archinstall archiso atftp autoconf automake autorandr avahi awesome-terminal-fonts b43-fwcutter base base-devel bash bash-completion bc bind bind-tools bison blueman bluez bluez-libs bluez-plugins bluez-tools bluez-utils bolt boost boost-libs bridge-utils brightnessctl brltty broadcom-wl btrfs-progs bzip2 bzip3 cargo ccache cifs-utils clang clonezilla cloud-init cmake conky cpupower crda cronie cryptsetup ctags cups cups-filters cups-pdf curl darkhttpd ddrescue devtools dex dhclient dhcpcd dialog diffutils dmidecode dmraid dnscrypt-proxy dnsmasq dnsutils docker docker-buildx docker-compose docker-machine dolphin dolphin-plugins dosfstools e2fsprogs edk2-shell efibootmgr espeakup ethtool exa exfatprogs expac eza f2fs-tools fakeroot fatresize fd feh ffmpeg ffmpegthumbnailer flameshot flex foomatic-db firefox foomatic-db-engine foot-terminfo fsarchiver fwbuilder gammu gawk gcc gcc-libs gdb ghostscript git git-lfs gnome-keyring gnu-netcat gnupg go gpart gparted gpm gptfdisk grub grub-customizer gsfonts gst-libav gst-plugin-pipewire gst-plugins-bad gst-plugins-good gst-plugins-ugly gstreamer gtk-update-icon-cache gufw gutenprint gvfs gvfs-afc gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-smb gzip haveged hdparm helix highlight htop hyperv inotify-tools intel-ucode ipython irqbalance irssi iw iwd jasper jfsutils kdeconnect kitty-terminfo kvantum less lftp libavif libde265 libdv libfido2 libheif libmpeg2 libreoffice-fresh libtheora libusb-compat libvpx libwebp libx11 libxext libxinerama lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lightdm-webkit2-greeter linux linux-atm linux-firmware linux-firmware-marvell linux-headers llvm logrotate lrzip lsb-release lsof lsscsi lua lvm2 lynx lz4 lzip lzop make man-db man-pages mc mdadm memtest86+ memtest86+-efi mercurial menumaker mercurial mesa mkinitcpio mkinitcpio-archiso mkinitcpio-nfs-utils mobile-broadband-provider-info modemmanager moreutils most mousepad mousetweaks mpv mtools nano nbd ncdu ndisc6 neovim net-tools nethogs network-manager-applet network-manager-sstp networkmanager networkmanager-l2tp networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-strongswan networkmanager-vpnc nfs-utils nilfs-utils nm-cloud-setup nm-connection-editor nmap npm nss-mdns ntfs-3g ntp numlockx nvme-cli open-iscsi openconnect openldap openpgp-card-tools openssh openvpn os-prober otf-libertinus p7zip pacman-contrib parole partclone parted partimage patch pavucontrol pcsclite perl picom pipewire pipewire-alsa pipewire-jack pipewire-pulse pkgconf pkgfile plocate plymouth power-profiles-daemon powertop ppp pptpclient profile-sync-daemon pulsemixer pv python-docker qt5-tools qt5ct ranger reflector reiserfsprogs rfkill ristretto rofi rofi-emoji rp-pppoe rsync rtorrent ruby rustup rxvt-unicode-terminfo schroedinger screen scrot sdparm sed sequoia-sq sg3_utils smartmontools smbclient syncthing socat sof-firmware squashfs-tools strace sudo syslinux systemd-resolvconf tar tcpdump terminus-font testdisk tex-gyre-fonts thermald thunar-archive-plugin thunar-media-tags-plugin thunar-volman thunderbird timeshift tldr tmux tor tpm2-tools tpm2-tss traceroute trash-cli tree ttf-fira-code ttf-hack-nerd ttf-jetbrains-mono-nerd ttf-ubuntu-font-family tumbler udftools udisks2 ueberzug ufw unace unarchiver unrar unzip upower usb_modeswitch usbmuxd usbutils virtualbox virtualbox-guest-utils virtualbox-host-modules-arch virtualbox-ext-vnc virtualbox-guest-iso vim vlc vpnc webkit2gtk wezterm-terminfo wget wireguard-tools wireless_tools wireless-regdb wireplumber wpa_supplicant wvdial x264 x265 xarchiver xclip xcompmgr xdg-desktop-portal-gtk xdg-desktop-portal-xapp xdg-user-dirs xdg-user-dirs-gtk xdotool xfburn xfce4 xfce4-battery-plugin xfce4-clipman-plugin xfce4-cpufreq-plugin xfce4-cpugraph-plugin xfce4-dict xfce4-diskperf-plugin xfce4-eyes-plugin xfce4-fsguard-plugin xfce4-genmon-plugin xfce4-goodies xfce4-mount-plugin xfce4-mpc-plugin xfce4-netload-plugin xfce4-notes-plugin xfce4-notifyd xfce4-power-manager xfce4-pulseaudio-plugin xfce4-screensaver xfce4-screenshooter xfce4-sensors-plugin xfce4-smartbookmark-plugin xfce4-systemload-plugin xfce4-taskmanager xfce4-time-out-plugin xfce4-timer-plugin xfce4-verve-plugin xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin xfsprogs xl2tpd xmlto xorg xorg-apps xorg-server xorg-xinit xsensors xvidcore xz yaml-cpp zip zsh zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-syntax-highlighting zstd
-sleep 5s
+sleep 6s
 clear
 
 # Install pkgs and tools by AUR..
@@ -547,14 +547,14 @@ git clone https://aur.archlinux.org/yay-bin.git /etc/skel/aur-pkg/yay-bin
 cd /etc/skel/aur-pkg/yay-bin || exit
 makepkg -sic --noconfirm
 cd - || exit
-sleep 5s
+sleep 6s
 clear
 
 # Install pkgs from yay-bin
 echo "Installing pkgs from yay-bin"
-sleep 5s
-yay -S --noconfirm --needed airdroid-nativefier android-sdk-platform-tools appmenu-gtk-module-git appmenu-qt4 bluez-firmware brave-bin caffeine-ng dolphin-megasync-bin downgrade eww-x11 fancontrol-gui firebuild gtk3-nocsd-git libdbusmenu-glib libdbusmenu-gtk2 libdbusmenu-gtk3 mkinitcpio-firmware mkinitcpio-numlock mugshot nbfc obsidian-bin ocs-url portmaster-stub-bin repoctl rtl8821ce-dkms-git rtw89-dkms-git thunar-extended thunar-megasync-bin thunar-secure-delete thunar-shares-plugin thunar-vcs-plugin universal-android-debloater-bin vala-panel-appmenu-common-git vala-panel-appmenu-registrar-git vala-panel-appmenu-xfce-git visual-studio-code-bin xfce4-docklike-plugin xfce4-panel-profiles zsh-theme-powerlevel10k-gitS
-sleep 5s
+sleep 6s
+yay -S --noconfirm --needed airdroid-nativefier android-sdk-platform-tools appmenu-gtk-module-git appmenu-qt4 bluez-firmware brave-bin caffeine-ng dolphin-megasync-bin downgrade eww-x11 fancontrol-gui firebuild gtk3-nocsd-git libdbusmenu-glib libdbusmenu-gtk2 libdbusmenu-gtk3 mkinitcpio-firmware mkinitcpio-numlock mugshot nbfc obsidian-bin ocs-url portmaster-stub-bin repoctl rtl8821ce-dkms-git rtw89-dkms-git thunar-extended thunar-megasync-bin thunar-secure-delete thunar-shares-plugin thunar-vcs-plugin universal-android-debloater-bin vala-panel-appmenu-common-git vala-panel-appmenu-registrar-git vala-panel-appmenu-xfce-git visual-studio-code-bin xfce4-docklike-plugin xfce4-panel-profiles zsh-theme-powerlevel10k-git stacer-bin
+sleep 6s
 clear
 
 # Setting boot partition "EFI"
@@ -566,7 +566,7 @@ if [ ! -d "$efidirectory" ]; then
   mkdir -p "$efidirectory"
 fi
 mount "$efipartition" "$efidirectory"
-sleep 5s
+sleep 6s
 clear
 
 # Install grub
@@ -575,7 +575,7 @@ printf "\n"
 echo "Installing grub bootloader in /boot/efi parttiton"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
-sleep 5s
+sleep 6s
 
 # os-prober
 read -p "Are you duelbooting? [Y/n]: " use_os_prober
@@ -583,19 +583,19 @@ if [[ $use_os_prober =~ ^[Yy]$ ]]; then
   echo "Enabling os-prober..."
   sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
   os-prober
-  sleep 5s
+  sleep 6s
   grub-mkconfig -o /boot/grub/grub.cfg
 else
   printf "\n"
   echo "os-prober not enabled."
 fi
-sleep 5s
+sleep 6s
 clear
 
 # Setting root user
 echo "Enter password for root user: "
 passwd
-sleep 5s
+sleep 6s
 clear
 
 # Setting regular user
@@ -605,58 +605,63 @@ read -p "Enter username to add a regular user: " username
 useradd -m -G wheel -s /bin/zsh "$username"
 echo "Enter password for '$username': "
 passwd "$username"
-sleep 5s
+sleep 6s
 clear
 
 # Adding sudo previliages to the user you created
 echo "NOTE: ALWAYS REMEMBER THIS USERNAME AND PASSWORD YOU PUT JUST NOW."
 printf "\n"
-sleep 5s
+sleep 6s
 echo "Giving sudo access to '$username'!"
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
 clear
-sleep 5s
+sleep 6s
 
 #edit sudo
 echo 'Defaults env_keep += "HOME"' | tee -a /etc/sudoers
-sleep 5s
+sleep 6s
 clear
 
 # Personal dotfiles
 echo "Setting up Personal dotfiles"
 git clone https://github.com/MikuX-Dev/dotfiles.git /etc/skel/.dotfiles
-sleep 5s
+sleep 6s
 
 # Creating folder first
-mkdir -p /etc/skel/.config/
+echo "Creating folder's..."
+mkdir -p /root/bin/
 mkdir -p /etc/skel/bin/
+mkdir -p /etc/skel/.config/
 mkdir -p /usr/share/lightdm-webkit/themes/glorious
-sleep 5s
+sleep 6s
 
 printf "\n"
 echo "Setting up shell"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp -r /etc/skel/.dotfiles/shell/bash/* /etc/skel/
+# cp -r /etc/skel/.dotfiles/shell/bash/* /etc/skel/
 cp -r /etc/skel/.dotfiles/shell/zsh/* /etc/skel/
-cp -r /etc/skel/.dotfiles/shell/zsh/* /root/
 cp -r /etc/skel/.dotfiles/shell/p-script/* /etc/skel/bin/
+cp -r /etc/skel/.dotfiles/shell/zsh/* /root/
+cp -r /etc/skel/.dotfiles/shell/zsh/* /root/
+# cp -r /etc/skel/.dotfiles/shell/
+cp -r /etc/skel/.dotfiles/shell/p-script/* /root/bin/
 chsh -s /bin/zsh root
-sleep 5s
+sleep 6s
 
 printf "\n"
 echo "Setting up config's"
 cp -r /etc/skel/.dotfiles/config/* /etc/skel/.config/
 cp -r /etc/skel/.dotfiles/local/share/* /etc/skel/.local/share/
-sleep 5s
+sleep 6s
 
 printf "\n"
-echo "Setting up themes,..."
+echo "Setting up themes..."
 cp -r /etc/skel/.dotfiles/themes/themes/* /usr/share/themes/
 cp -r /etc/skel/.dotfiles/themes/icons/* /usr/share/icons/
 cp -r /etc/skel/.dotfiles/themes/plymouth/* /etc/plymouth/
 cp -r /etc/skel/.dotfiles/wallpaper/* /usr/share/backgrounds/
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf /usr/share/fonts/
-sleep 5s
+sleep 6s
 
 printf "\n"
 echo "Configuring plymouth"
@@ -666,7 +671,7 @@ mkinitcpio -p linux
 grub-mkconfig -o /boot/grub/grub.cfg
 plymouth-set-default-theme -R archfiery
 grub-mkconfig -o /boot/grub/grub.cfg
-sleep 5s
+sleep 6s
 
 printf "\n"
 echo "Setting up lightdm"
@@ -676,31 +681,34 @@ tar -zxvf glorious-latest.tar.gz -C /usr/share/lightdm-webkit/themes/glorious --
 sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf
 sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = glorious #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
 sed -i 's/^debug_mode\s*=\s*\(.*\)/debug_mode = true #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
-sleep 5s
+sleep 6s
+clear
 
 echo "Installing grub-theme"
 printf "\n"
 cp -r /etc/skel/.dotfiles/themes/grub/themes/* /usr/share/grub/themes/
 sed -i 's/#GRUB_THEME="/path/to/gfxtheme"/GRUB_THEME="/usr/share/grub/themes/archfiery/theme.txt"/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-sleep 5s
+sleep 6s
+clear
 
 # Setting up virtualbox
 echo "Setting up virtualbox"
 modprobe vboxdrv
 gpasswd -a "$username" vboxusers
-sleep 5s
+sleep 6s
+clear
 
 # Enable services
 echo "Enabling services.."
 printf "\n"
 enable_services=('irqbalance.service' 'udisks2.service' 'httpd.service' 'cronie.service' 'sshd.service' 'lightdm-plymouth.service' 'NetworkManager.service' 'cups.service' 'bluetooth' 'ntpd.service' 'dhcpcd.service')
 systemctl enable "${enable_services[@]}"
-sleep 5s
+sleep 6s
 clear
 
 # Clearcache
 echo "Clearing cache..."
 yay -Scc --noconfirm
-sleep 5s
+sleep 6s
 clear
