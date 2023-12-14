871b498394a9f4a630401196c1241245699617f5
MikuX-Dev <abinishanth3464@gmail.com>
2023-12-14 09:04:56 +0530

    added --noconfirm un blackarch

--- test.sh
+++ test.sh
@@ -611,7 +611,7 @@ if [ -d "/sys/firmware/efi/efivars" ]; then
   echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
   chmod +x strap.sh
   ./strap.sh
-  pacman -Syyu
+  pacman -Syyu --noconfirm --needed
   rm -rf strap.sh
   clear
 
